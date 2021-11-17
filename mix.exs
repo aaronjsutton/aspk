b53ddeaf8293e87a7446f2de72c640af6d240985
Aaron Sutton <hey@aaron.as>
2021-11-17 18:36:38 -0500

    Add release bootstrap code and fix data/

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ASPK.MixProject do
   def project do
     [
       app: :aspk,
-      version: "0.1.0",
+      version: "0.7.1",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
